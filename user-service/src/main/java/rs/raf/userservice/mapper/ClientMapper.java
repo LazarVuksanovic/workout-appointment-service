cf0e77c31aa37e910d5ec9477ca90a47fa87af8e
LazarVuksanovic <lazar.vuksanovic17@gmail.com>
2023-12-21 15:59:01 +0100

    mappers

---
+++ user-service/src/main/java/rs/raf/userservice/mapper/ClientMapper.java
@@ -0,0 +1,53 @@
+package rs.raf.userservice.mapper;
+
+import org.springframework.stereotype.Component;
+import rs.raf.userservice.domain.Client;
+import rs.raf.userservice.dto.ClientCreateDto;
+import rs.raf.userservice.dto.ClientDto;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+@Component
+public class ClientMapper {
+
+    public ClientDto clientToClientDto(Client client) {
+        ClientDto clientDto = new ClientDto();
+        clientDto.setId(client.getId());
+        clientDto.setEmail(client.getEmail());
+        clientDto.setFirstName(client.getFirstName());
+        clientDto.setLastName(client.getLastName());
+        clientDto.setUsername(client.getUsername());
+        clientDto.setDateOfBirth(client.getDateOfBirth());
+        clientDto.setMembershipCardNumber(client.getMembershipCardNumber());
+        clientDto.setScheduledTrainings(client.getScheduledTrainings());
+        return clientDto;
+    }
+
+    public Client clientCreateDtoToClient(ClientCreateDto clientCreateDto) {
+        Client client = new Client();
+        client.setEmail(clientCreateDto.getEmail());
+        client.setFirstName(clientCreateDto.getFirstName());
+        client.setLastName(clientCreateDto.getLastName());
+        client.setUsername(clientCreateDto.getUsername());
+        client.setPassword(clientCreateDto.getPassword());
+        client.setDateOfBirth(clientCreateDto.getDateOfBirth());
+        client.setMembershipCardNumber(generateMembershipCard(clientCreateDto));
+        client.setScheduledTrainings(0);
+        return client;
+    }
+
+    private String generateMembershipCard(ClientCreateDto clientCreateDto) {
+        String initials = clientCreateDto.getFirstName().substring(0, 1).toUpperCase() +
+                clientCreateDto.getLastName().substring(0, 1).toUpperCase();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMyy");
+        String birthDate = clientCreateDto.getDateOfBirth().format(formatter);
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(90 - 10) + 10; // Generisanje random dvocifrenog broja
+
+        return initials + birthDate + randomNumber;
+    }
+}
---
+++ user-service/src/main/java/rs/raf/userservice/mapper/GymManagerMapper.java
@@ -0,0 +1,39 @@
+package rs.raf.userservice.mapper;
+
+import org.springframework.stereotype.Component;
+import rs.raf.userservice.domain.GymManager;
+import rs.raf.userservice.dto.ClientDto;
+import rs.raf.userservice.dto.GymManagerCreateDto;
+import rs.raf.userservice.dto.GymManagerDto;
+
+import java.time.LocalDate;
+
+@Component
+public class GymManagerMapper {
+
+    public GymManagerDto gymManagerToGymManagerDto(GymManager gymManager){
+        GymManagerDto gymManagerDto = new GymManagerDto();
+        gymManagerDto.setId(gymManager.getId());
+        gymManagerDto.setEmail(gymManager.getEmail());
+        gymManagerDto.setFirstName(gymManager.getFirstName());
+        gymManagerDto.setLastName(gymManager.getLastName());
+        gymManagerDto.setUsername(gymManager.getUsername());
+        gymManagerDto.setDateOfBirth(gymManager.getDateOfBirth());
+        gymManagerDto.setGymName(gymManager.getGymName());
+        gymManagerDto.setEmploymentDate(gymManager.getEmploymentDate());
+        return gymManagerDto;
+    }
+
+    public GymManager gymManagerCreateDtoToGymManager(GymManagerCreateDto gymManagerCreateDto){
+        GymManager gymManager = new GymManager();
+        gymManager.setEmail(gymManagerCreateDto.getEmail());
+        gymManager.setPassword(gymManagerCreateDto.getPassword());
+        gymManager.setFirstName(gymManagerCreateDto.getFirstName());
+        gymManager.setLastName(gymManagerCreateDto.getLastName());
+        gymManager.setUsername(gymManagerCreateDto.getUsername());
+        gymManager.setDateOfBirth(gymManagerCreateDto.getDateOfBirth());
+        gymManager.setGymName(gymManagerCreateDto.getGymName());
+        gymManager.setEmploymentDate(LocalDate.now());
+        return gymManager;
+    }
+}
