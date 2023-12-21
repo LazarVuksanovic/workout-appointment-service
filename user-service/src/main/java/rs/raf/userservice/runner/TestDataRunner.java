3a48225c71b553869b0f3fd4205fdd3d02fe0395
LazarVuksanovic <lazar.vuksanovic17@gmail.com>
2023-12-21 22:33:03 +0100

    runner

---
+++ user-service/src/main/java/rs/raf/userservice/runner/TestDataRunner.java
@@ -0,0 +1,28 @@
+package rs.raf.userservice.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import rs.raf.userservice.domain.Admin;
+import rs.raf.userservice.repository.AdminRepository;
+
+@Profile({"default"})
+@Component
+public class TestDataRunner implements CommandLineRunner {
+
+    private AdminRepository adminRepository;
+
+    public TestDataRunner(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        //unosimo admina
+        Admin admin = new Admin();
+        admin.setEmail("lvuksanovic1021rn@raf.rs");
+        admin.setPassword("admin");
+        admin.setUsername("admin");
+        this.adminRepository.save(admin);
+    }
+}
