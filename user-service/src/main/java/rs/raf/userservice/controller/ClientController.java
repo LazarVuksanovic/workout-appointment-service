f6d6e852769d5b4eb2ffe89fe50444bdbdc7deb0
LazarVuksanovic <lazar.vuksanovic17@gmail.com>
2023-12-21 16:00:01 +0100

    controllers

---
+++ user-service/src/main/java/rs/raf/userservice/controller/ClientController.java
@@ -0,0 +1,36 @@
+package rs.raf.userservice.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rs.raf.userservice.dto.ClientCreateDto;
+import rs.raf.userservice.dto.ClientDto;
+import rs.raf.userservice.service.ClientService;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/user/client")
+public class ClientController {
+
+    private ClientService clientService;
+
+    public ClientController(ClientService clientService){ this.clientService = clientService; }
+
+    @ApiOperation(value = "find all clients")
+    @GetMapping
+    public ResponseEntity<Page<ClientDto>> findAll(Pageable pageable){
+        return new ResponseEntity<>(this.clientService.findAll(pageable), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Register client")
+    @PostMapping
+    public ResponseEntity<ClientDto> saveClient(@RequestBody @Valid ClientCreateDto clientCreateDto) {
+        return new ResponseEntity<>(this.clientService.add(clientCreateDto), HttpStatus.CREATED);
+    }
+}
---
+++ user-service/src/main/java/rs/raf/userservice/controller/GymManagerController.java
@@ -0,0 +1,34 @@
+package rs.raf.userservice.controller;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rs.raf.userservice.dto.GymManagerCreateDto;
+import rs.raf.userservice.dto.GymManagerDto;
+import rs.raf.userservice.service.GymManagerService;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/user/gymmanager")
+public class GymManagerController {
+
+    private GymManagerService gymManagerService;
+
+    public GymManagerController(GymManagerService gymManagerService){ this.gymManagerService = gymManagerService; }
+
+    @ApiOperation(value = "find all gym managers")
+    @GetMapping
+    public ResponseEntity<Page<GymManagerDto>> findAll(Pageable pageable){
+        return new ResponseEntity<>(this.gymManagerService.findAll(pageable), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "register gym manager")
+    @PostMapping
+    public ResponseEntity<GymManagerDto> saveGymManager(@RequestBody @Valid GymManagerCreateDto gymManagerCreateDto){
+        return new ResponseEntity<>(this.gymManagerService.add(gymManagerCreateDto), HttpStatus.OK);
+    }
+}
