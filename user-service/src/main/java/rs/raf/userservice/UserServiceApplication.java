5be73aa3a765427812b96ca32ef14260e263f054
LazarVuksanovic <lazar.vuksanovic17@gmail.com>
2023-12-21 15:53:28 +0100

    app

---
+++ user-service/src/main/java/rs/raf/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package rs.raf.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+    //http://localhost:8080/api/swagger-ui.html
+}
