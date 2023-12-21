cf023a4636f3f85c8f33674628b26a0f52f49d9d
LazarVuksanovic <lazar.vuksanovic17@gmail.com>
2023-12-22 00:04:31 +0100

    user repo

--- user-service/src/main/java/rs/raf/userservice/repository/ClientRepository.java
+++ user-service/src/main/java/rs/raf/userservice/repository/ClientRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import rs.raf.userservice.domain.Client;
 
+import java.util.Optional;
+
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    Optional<Client> findUserByUsernameAndPassword(String username, String password);
 }
--- user-service/src/main/java/rs/raf/userservice/repository/UserRepository.java
+++ user-service/src/main/java/rs/raf/userservice/repository/UserRepository.java
@@ -2,8 +2,13 @@ package rs.raf.userservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import rs.raf.userservice.domain.Client;
 import rs.raf.userservice.domain.User;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByUsernameAndPassword(String username, String password);
 }
