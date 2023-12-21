b087d4bdb80af13c2abb6dc11b9c226763b6d006
LazarVuksanovic <lazar.vuksanovic17@gmail.com>
2023-12-22 00:01:27 +0100

    token dto

---
+++ user-service/src/main/java/rs/raf/userservice/dto/TokenRequestDto.java
@@ -0,0 +1,20 @@
+package rs.raf.userservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TokenRequestDto {
+
+    private String username;
+    private String password;
+
+    public TokenRequestDto() {
+    }
+
+    public TokenRequestDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
---
+++ user-service/src/main/java/rs/raf/userservice/dto/TokenResponseDto.java
@@ -0,0 +1,18 @@
+package rs.raf.userservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TokenResponseDto {
+
+    private String token;
+
+    public TokenResponseDto() {
+    }
+
+    public TokenResponseDto(String token) {
+        this.token = token;
+    }
+}
