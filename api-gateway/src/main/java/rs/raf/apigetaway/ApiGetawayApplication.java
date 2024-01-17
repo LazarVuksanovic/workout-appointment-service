17f8011a1d8f889fc0ba7a67205797891c18470a
LazarVuksanovic <lazar.vuksanovic17@gmail.com>
2024-01-17 06:07:59 +0100

    zuul

--- api-gateway/src/main/java/rs/raf/apigetaway/ApiGetawayApplication.java
+++ api-gateway/src/main/java/rs/raf/apigetaway/ApiGetawayApplication.java
@@ -1,32 +1,13 @@
 package rs.raf.apigetaway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGetawayApplication {
 
-	@Bean
-	public FilterRegistrationBean corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
-		source.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-		bean.setOrder(0);
-		return bean;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGetawayApplication.class, args);
